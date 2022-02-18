532417a87d99c6b60b902f63313e3c99aa55113f
c0venn <fabichidori2@gmail.com>
2022-02-18 01:50:38 +0000

    adding responsive and center div

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,22 +6,26 @@ import React, { useState } from "react";
 export function Home() {
 	const [selectedColor, setSelectedColor] = useState("red");
 	return (
-		<div className="traffic-light">
-			<div
-				onClick={() => setSelectedColor("red")}
-				className={
-					"light red" + (selectedColor == "red" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor("yellow")}
-				className={
-					"light yellow" + (selectedColor == "yellow" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor("green")}
-				className={
-					"light green" + (selectedColor == "green" ? " glow" : "")
-				}></div>
+		<div className="center">
+			<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor == "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" +
+						(selectedColor == "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" +
+						(selectedColor == "green" ? " glow" : "")
+					}></div>
+			</div>
 		</div>
 	);
 }
